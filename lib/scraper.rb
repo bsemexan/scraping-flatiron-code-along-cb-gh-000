5b78bffec203a80c39420bd16a72cd7fc84b8a09
Berry  Semexan <bsemexan@gmail.com>
2018-02-21 21:57:15 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,6 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
-
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     binding.pry
